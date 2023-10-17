ef09ac6ee65ee7728ce07da02b60cbf7d53a31e6
Kevin <109120454+Klunam22@users.noreply.github.com>
2023-10-17 19:20:19 -0400

    Update Lottery.sol
    Changed the upkeep logic but still testing it to see if the transfer of funds works

--- packages/hardhat/contracts/Lottery.sol
+++ packages/hardhat/contracts/Lottery.sol
@@ -1,10 +1,12 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
 
+contract Lottery is AutomationCompatibleInterface {
+    // event
+    event WinnerPicked(address winner, uint256 amount);
 
-contract Lottery is AutomationCompatibleInterface  {
+
 
     // state variables:
     address public owner;
@@ -13,58 +15,54 @@ contract Lottery is AutomationCompatibleInterface  {
     uint public lotteryEndTime;
     mapping (uint => address payable) public lotteryHistory;
 
-     /**
-     * Use an interval in seconds and a timestamp to slow execution of Upkeep
-     */
+    // Use an interval in seconds and a timestamp to slow execution of Upkeep
     uint public immutable interval;
     uint public lastTimeStamp;
 
     constructor(uint updateInterval) {
         interval = updateInterval;
         lastTimeStamp = block.timestamp;
-
-
         owner = msg.sender;
         lotteryId = 1;
         // Set the lottery end time to 5 minutes from deployment
         lotteryEndTime = block.timestamp + 5 minutes;
     }
-     // Chainlink Functions
-    function checkUpkeep(
-        bytes calldata /* checkData */
-    )
-        external
-        view
-        override
-        returns (bool upkeepNeeded, bytes memory /* performData */)
-    {
+
+    // Chainlink Functions
+    function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory /* performData */) {
         upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
         // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
     }
 
-   function performUpkeep(bytes calldata /* performData */) external override {
-    if ((block.timestamp - lastTimeStamp) > interval) {
-        lastTimeStamp = block.timestamp;
+    function performUpkeep(bytes calldata /* performData */) external override {
+        if ((block.timestamp - lastTimeStamp) > interval) {
+            lastTimeStamp = block.timestamp;
 
-        // PickWinner Logic:
-        uint index = getRandomNumber() % players.length;
-        players[index].transfer(address(this).balance); 
-        lotteryHistory[lotteryId] = players[index];
-        lotteryId++;
+            pickWinner();
+            
+            // reset the state of the contract
+            players = new address payable[](0);
+            
+            // Check if there are enough players for the new lottery
+            //require(players.length > 0, "Not enough participants for the new lottery");
 
-        // reset the state of the contract
-        players = new address payable[](0);
-        
-        // Check if there are enough players for the new lottery
-        require(players.length > 0, "Not enough participants for the new lottery");
-
-        // Check if the lotteryEndTime + 10 minutes has passed
-        if (block.timestamp > lotteryEndTime + 10 minutes) {
-            // Start a new lottery
-            lotteryEndTime = block.timestamp + 5 minutes;
+            // Check if the lotteryEndTime + 10 minutes has passed
+            if (block.timestamp > lotteryEndTime + 10 minutes) {
+                // Start a new lottery
+                lotteryEndTime = block.timestamp + 5 minutes;
+            }
         }
     }
-   }
+
+        // Functions
+
+    function getNextLotteryStartTime() public view returns (uint) {
+        if (block.timestamp > lotteryEndTime) {
+            return lotteryEndTime + 10 minutes;
+        } else {
+            return 0; // 0 indicates that the lottery has not ended yet
+        }
+    }
 
     // Get lotteryEndTime in readable numbers
     function getTimeLeft() public view returns (uint daysLeft, uint hoursLeft, uint minutesLeft) {
@@ -91,7 +89,9 @@ contract Lottery is AutomationCompatibleInterface  {
     function enterLottery() public payable {
         // require that after [lotteryEndTime]  players can no longer enter to this lottery
         require(block.timestamp < lotteryEndTime, "Lottery entry period has ended, Please enter current/new one");
-        require(msg.value > .01 ether, "Deposit must be greater > .01 ether to enter");
+        // require(msg.value > .01 ether, "Deposit must be greater > .01 ether to enter");
+        uint256 minimumWei = 10**16;
+        require(msg.value >= minimumWei, "Deposit must be greater than or equal to 0.01 ether in Wei to enter");
 
         // address of player entering lottery
         players.push(payable(msg.sender));
@@ -100,5 +100,26 @@ contract Lottery is AutomationCompatibleInterface  {
     function getRandomNumber() public view returns (uint) {
         return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
     }
+
+   
+function pickWinner() internal {
+    require(players.length > 0, "No participants in the lottery");
+    require(address(this).balance > 0, "Insufficient balance in the contract");
+
+    uint index = getRandomNumber() % players.length;
+    address payable winner = players[index];
+    uint256 amount = address(this).balance;
+
+    // Transfer funds to the winner
+    if (!winner.send(amount)) {
+        revert("Failed to transfer funds to the winner");
+    }
+
+    // Emit an event after the successful transfer
+    emit WinnerPicked(winner, amount);
+
+    // Add other necessary logic
+    // ...
+}
 
 }
